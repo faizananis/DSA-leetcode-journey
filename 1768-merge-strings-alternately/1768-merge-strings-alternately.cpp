9b4f0bba59342e416c62064fd7595104ef7f92c3
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-05-19 21:22:47 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,30 +3,17 @@ public:
     string mergeAlternately(string word1, string word2) {
         string str;
         int i=0;
-        int n=0;
-        while(word1[i]!='\0'&&word2[i]!='\0')
+        while(word1[i]!='\0'||word2[i]!='\0')
         {
-            str.push_back(word1[i]);
-            str.push_back(word2[i]);
-            i++;
-        }
-        if(word1[i]!='\0')
-        {
-            while(word1[i]!='\0')
+            if(word1[i]!='\0')
             {
                 str.push_back(word1[i]);
-                i++;
             }
-            return str;
-        }
-        if(word2[i]!='\0')
-        {
-            while(word2[i]!='\0')
+            if(word2[i]!='\0')
             {
                 str.push_back(word2[i]);
-                i++;
             }
-            return str;
+            i++;
         }
         return str;
     }
