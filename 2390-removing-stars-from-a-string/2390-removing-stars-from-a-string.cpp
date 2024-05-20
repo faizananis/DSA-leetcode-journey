bf5be90e5dbc8ea8b619436c84dd87faa14a2fa4
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-05-20 17:46:15 +0500

    Time: 89 ms (43.43%), Space: 27 MB (78.8%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int i=0;
+        string str;
+        int n=s.length();
+        while(i<n)
+        {
+            if(s[i]=='*')
+            {
+                if(!str.empty())
+                {
+                    str.pop_back();
+                }
+            }
+            else
+            {
+                str.push_back(s[i]);
+            }
+            i++;
+        }
+        return str;
+    }
+};
