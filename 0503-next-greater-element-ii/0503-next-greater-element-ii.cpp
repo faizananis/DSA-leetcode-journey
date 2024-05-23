ac1135ea9b532548d239141462667bdf42a21d79
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-05-24 03:45:10 +0500

    Time: 200 ms (5.08%), Space: 27.8 MB (14.98%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,19 +5,43 @@ public:
         int i=0;
         int j=0;
         int n=nums.size();
+        //stack<int> st;
         reverse(nums.begin(),nums.end());
-        for(i=0;i<n;i++)
-        {
-            nums.push_back(nums[i]);
-        }
+        // for(i=0;i<n;i++)
+        // {
+        //     nums.push_back(nums[i]);
+        // }
         for(i=0;i<n*2;i++)
         {
+            // if(st.empty())
+            // {
+            //     result.push_back(-1);
+            //     st.push(nums[i]);
+            // }
+            // else
+            // {
+            //     if(st.top()<nums[i])
+            //     {
+            //         st.pop();
+            //         while(st.top()<nums[i]&&!st.empty())
+            //         {
+            //             st.pop();
+            //         }
+            //         st.push(nums[i]);
+            //         result.push_back(nums[i]);
+            //     }
+            //     else
+            //     {
+            //         st.push(nums[i]);
+            //         result.push_back(nums[i]);
+            //     }
+            // }
             j=i;
             while(j>0)
             {
-                if(nums[i]<nums[j])
+                if(nums[i%n]<nums[j%n])
                 {
-                    result.push_back(nums[j]);
+                    result.push_back(nums[j%n]);
                     break;
                 }
                 j--;
