6639fb2a4808f52e2a4b10a2cf0ae494a813883c
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-06-23 12:13:01 +0500

    Time: 19 ms (37.57%), Space: 21.7 MB (38.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -6,9 +6,13 @@ public:
         int j=0;
         for(i=0;i<n;i++)
         {
-            if(nums[i]!=0)
+            if(j<i&&nums[i]!=0)
             {
                 nums[j]=nums[i];
+                j++;
+            }
+            else if(nums[i]!=0)
+            {
                 j++;
             }
         }
