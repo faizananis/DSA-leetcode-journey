5aab9dff102be50e89be10039b89ba6f8baf634d
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-08-28 16:49:06 +0500

    Time: 665 ms (69.17%), Space: 25.1 MB (6.82%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        j=0
+        maxlength=0
+        table={}
+        for i in range(len(fruits)):
+            if fruits[i] not in table:
+                table[fruits[i]]=1
+            else:
+                table[fruits[i]]+=1
+            if len(table)<=2:
+                maxlength=max(maxlength,i-j+1)
+            else:
+                table[fruits[j]]-=1
+                if table[fruits[j]]==0:
+                    table.pop(fruits[j])
+                j+=1
+        return maxlength
+
+            
+            
+                
+
