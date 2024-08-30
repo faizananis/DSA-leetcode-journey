aa09acb5a96ca8d7f996f6db5cc9385f52d50c33
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-08-30 17:00:25 +0500

    Time: 541 ms (98.29%), Space: 24.9 MB (7.01%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,21 +1,43 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        j=0
-        maxlength=0
-        table={}
-        for i in range(len(fruits)):
-            if fruits[i] not in table:
-                table[fruits[i]]=1
-            else:
-                table[fruits[i]]+=1
-            if len(table)<=2:
-                maxlength=max(maxlength,i-j+1)
-            else:
-                table[fruits[j]]-=1
-                if table[fruits[j]]==0:
-                    table.pop(fruits[j])
-                j+=1
-        return maxlength
+        i, j = 0, 0
+        n = len(fruits)
+        while j < n and fruits[i] == fruits[j]:
+            j += 1
+        if j == n:
+            return n
+
+        l, r = i, j + 1
+        ans = 0
+        while r < n:
+            if fruits[r] != fruits[j]:
+                if fruits[r] == fruits[i]:
+                    i = j
+                    j = r
+                else:
+                    ans = max(ans, r - l)
+                    i = j
+                    l = j
+                    j = r
+            r += 1
+        ans = max(ans, r - l)
+        return ans
+        # j=0
+        # maxlength=0
+        # table={}
+        # for i in range(len(fruits)):
+        #     if fruits[i] not in table:
+        #         table[fruits[i]]=1
+        #     else:
+        #         table[fruits[i]]+=1
+        #     if len(table)<=2:
+        #         maxlength=max(maxlength,i-j+1)
+        #     else:
+        #         table[fruits[j]]-=1
+        #         if table[fruits[j]]==0:
+        #             table.pop(fruits[j])
+        #         j+=1
+        # return maxlength
             
             
                 
