d410c0b85fab3ea4dbd083d69099365dc898315b
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-09-04 14:59:39 +0500

    Time: 35 ms (69.58%), Space: 16.4 MB (66.68%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -3,9 +3,8 @@ class Solution:
         freq=[0]*101
         count=0
         for i in nums:
-            if i in range(len(nums)):
-                freq[nums[i]]+=1
-        for i in freq:
-            if i!=0:
-                count+=((i*(i-1)))//2
+            freq[i]+=1
+        for j in freq:
+            if j>0:
+                count=count+(j*(j-1))//2
         return count
