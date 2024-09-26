f46e42b98e0e81011a9088cb637f11823db0380b
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-09-26 11:57:19 +0500

    Time: 189 ms (67.87%), Space: 18.1 MB (71.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -4,12 +4,11 @@ class Solution:
         right=len(nums)-1
         while left<=right:
             mid=(left+right)//2
-
             if nums[mid]==target:
                 return mid
-            elif nums[mid]<target:
+            
+            if nums[mid]<target:
                 left=mid+1
             else:
                 right=mid-1
-        
         return -1
