c00051a25fbf2a927752c5df05e692857776454b
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-10-25 04:53:30 +0500

    Time: 0 ms (100%), Space: 14.6 MB (61.08%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if(!root1 and !root2)
+        {
+            return true;
+        }
+        if(!root1 or !root2 or root1->val!=root2->val)
+        {
+            return false;
+        }
+        return flipEquiv(root1->left,root2->right) and flipEquiv(root1->right,root2->left) or 
+        flipEquiv(root1->left,root2->left) and flipEquiv(root1->right,root2->right);
+    }
+};
+
+
+
+
+
+
+    //     if(root1==root2)
+    //     {
+    //         return true;
+    //     }
+    //     if(root1==NULL && root2!=NULL)
+    //     {
+    //         return false;
+    //     }
+    //     if(root1!=NULL && root2==NULL)
+    //     {
+    //         return false;
+    //     }
+    //     return flipEquiv(root1->left,root2->right) and flipEquiv(root1->right,root2->left) or 
+    //     flipEquiv(root1->left,root2->left) and flipEquiv(root1->right,root2->right);
+        
+    //     if(root1->left==root2->right && root1->right!=NULL && root2->left!=NULL)
+    //     {
+    //         if(root1->right->val==root2->left->val)
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //     if(root1->right==root2->right && root1->left!=NULL && root2->left!=NULL)
+    //     {
+    //         if(root1->left->val==root2->left->val)
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //     if(root1->left==root2->left && root1->right==root2->right)
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+//};
