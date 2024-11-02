6b8c4336ccb7631141538c75982cde87075dd9d9
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-11-02 05:49:51 +0500

    Time: 0 ms (100%), Space: 16.5 MB (80.88%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        s=sentence.split()
+        for i in range(len(s)):
+            if s[i-1][-1]!=s[i][0]:
+                return False
+        return True
