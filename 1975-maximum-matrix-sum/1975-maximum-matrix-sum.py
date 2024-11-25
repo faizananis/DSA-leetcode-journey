58938ecf2bf13cdff87572f35e5e46a7dea3dd91
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-11-25 07:25:17 +0500

    Update 1975-maximum-matrix-sum.py

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -1,27 +1,5 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        # m = len(matrix)
-        # n = len(matrix[0])
-
-        # min_val = float('inf')
-        # count_neg = 0
-        # total_sum = 0
-
-        # for i in range(m):
-        #     for j in range(n):
-        #         total_sum += abs(matrix[i][j])
-        #         min_val = min(min_val, abs(matrix[i][j]))
-
-        #         if matrix[i][j] < 0:
-        #             count_neg += 1
-
-        # if count_neg % 2 == 0:
-        #     return total_sum
-        # return total_sum - 2 * min_val
-
-
-
-
         m=len(matrix)
         n=len(matrix[0])
         count_negative=0
@@ -41,11 +19,10 @@ class Solution:
             if count_negative%2==1:
                 for i in range(m):
                     for j in range(n):
-                        minimum=min(minimum,abs(matrix[j][j]))
+                        minimum=min(minimum,abs(matrix[i][j]))
         for i in range(m):
             for j in range(n):
                 sums+=abs(matrix[i][j])
         if minimum!=10000000:
             sums=sums-minimum*2
         return sums
-
