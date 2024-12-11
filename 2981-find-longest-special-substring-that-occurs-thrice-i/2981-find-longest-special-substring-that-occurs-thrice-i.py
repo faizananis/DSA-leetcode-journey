cfc00a04474691e9473d2d654a6d2b75f1edff60
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-12-11 10:52:24 +0500

    Time: 13 ms (66.75%), Space: 17.2 MB (19.74%) - LeetHub

---
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        top3freq = [[-1, -1, -1] for _ in range(26)]
+
+        last_seen = '*'
+        win_len = 0
+
+        # Find top-3 substring lengths for each character (a to z)
+        for curr in s:
+            idx = ord(curr) - ord('a')
+            win_len = win_len + 1 if curr == last_seen else 1
+            last_seen = curr
+
+            min_index = top3freq[idx].index(min(top3freq[idx]))
+            if win_len > top3freq[idx][min_index]:
+                top3freq[idx][min_index] = win_len
+
+        # Find the maximum length of substrings
+        max_len = -1
+        for freq in top3freq:
+            max_len = max(max_len, min(freq))
+
+        return max_len
+            
