5807d2310b7747ab7f11a80d88ccc24895c38195
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-12-11 17:33:02 +0500

    Time: 172 ms (85.47%), Space: 28.1 MB (39.52%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums=sorted(nums)
+        left=0
+        right=0
+        ans=right-left
+        flag=False
+        for right in range(len(nums)):
+            if nums[right]-nums[left]>2*k:
+                left+=1
+            else:
+                ans=max(ans,right-left+1)
+        return ans
