75e51e8b8308c979b31e3a89d8fbbb9b374c4825
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2024-12-26 22:19:11 +0500

    Time: 0 ms (100%), Space: 19.8 MB (7.75%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -8,12 +8,14 @@ class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
         ans=[]
         queue=deque()
-        largest=-2**31-1
-        queue.append(root)
+        largest=-2**31-1 #-("inf")
+        if root:
+            queue.append(root)
         while queue:
             largest=-2**31-1
             for _ in range(len(queue)):
-                node=queue.popleft()
+                if queue:
+                    node=queue.popleft()
                 if node.left:
                     queue.append(node.left)
                 if node.right:
