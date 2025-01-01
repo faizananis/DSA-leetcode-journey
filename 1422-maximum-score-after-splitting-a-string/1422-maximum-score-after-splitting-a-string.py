367b7a7f9fc05da621b1f248d0381a5eeac89958
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-01-01 22:59:26 +0500

    Time: 1 ms (68.37%), Space: 18 MB (9.48%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -25,7 +25,7 @@ class Solution:
         for n in s:
             if n=='1':
                 one+=1
-        for i in range(len(s)):
+        for i in range(len(s)-1):
             if s[i]=='0':
                 zero+=1
             else:
