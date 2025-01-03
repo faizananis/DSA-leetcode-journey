ae09d047b74414ff8cc810b7f820cba7efa7e43b
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-01-03 23:18:10 +0500

    Time: 27 ms (93.33%), Space: 18.6 MB (10.53%) - LeetHub

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallestRangeII(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        #return (nums[-1]-k)-(nums[0]+k) 
+        mi, ma = nums[0], nums[-1]
+        ans = ma - mi
+        for i in range(len(nums) - 1):
+            a, b = nums[i], nums[i+1]
+            ans = min(ans, max(ma-k, a+k) - min(mi+k, b-k))
+        return ans
+        
