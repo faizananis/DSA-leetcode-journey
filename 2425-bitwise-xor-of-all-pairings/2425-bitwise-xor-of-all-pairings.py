31d0564df6dd700d3f917fea74c3ac3326caba8d
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-01-17 01:58:40 +0500

    Time: 3 ms (73.99%), Space: 36.5 MB (61.27%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -3,15 +3,10 @@ class Solution:
         ans=0
         if len(nums1)&1==0 and len(nums2)&1==0:
             return 0  
-        if len(nums1)&1==0:
-            for n in nums1:
-                ans^=n
-        elif len(nums2)&1==0:
+        if len(nums1)&1:
             for n in nums2:
                 ans^=n
-        else:
+        if len(nums2)&1:
             for n in nums1:
-                ans^=n
-            for n in nums2:
                 ans^=n
         return ans
