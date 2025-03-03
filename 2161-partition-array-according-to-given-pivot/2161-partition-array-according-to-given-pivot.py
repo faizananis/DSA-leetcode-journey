1ab5964f2a8c49988e1ed31845b3f67f8ea759bc
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-03-04 04:27:11 +0500

    Time: 77 ms (8.89%), Space: 34.7 MB (33.41%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -11,3 +11,53 @@ class Solution:
             if nums[k]>pivot:
                 result.append(nums[k])
         return result
+
+
+        # small=0
+        # temp=0
+        # for i in range(len(nums)):
+        #     if nums[i]<pivot:
+        #         temp=nums[i]
+        #         nums[i]=nums[small]
+        #         nums[small]=temp
+        #         small+=1
+        # equal=small
+        # if equal<len(nums):
+        #     for i in range(equal,len(nums)):
+        #         if nums[i]==pivot:
+        #             temp=nums[i]
+        #             nums[i]=nums[equal]
+        #             nums[equal]=temp
+        #             equal+=1
+        # large=equal
+        # if large<len(nums):
+        #     for i in range(large,len(nums)):
+        #         if nums[i]>pivot:
+        #             temp=nums[i]
+        #             nums[i]=nums[large]
+        #             nums[large]=temp
+        #             large+=1
+        # return nums
+            
+
+        # n = len(nums)
+        # res = [0] * n
+        
+        # left, right = 0, n - 1
+        # left_res, right_res = 0, n - 1
+        
+        # while left < n:
+        #     if nums[left] < pivot:
+        #         res[left_res] = nums[left]
+        #         left_res += 1
+        #     if nums[right] > pivot:
+        #         res[right_res] = nums[right]
+        #         right_res -= 1
+        #     left += 1
+        #     right -= 1
+        
+        # while left_res <= right_res:
+        #     res[left_res] = pivot
+        #     left_res += 1
+        
+        # return res
