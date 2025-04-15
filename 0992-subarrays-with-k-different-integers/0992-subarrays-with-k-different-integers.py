15b1cd93f517d438a0b757197c72bed8d74a1fe3
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-04-16 01:05:56 +0500

    Time: 119 ms (75.18%), Space: 19.6 MB (95.45%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -3,17 +3,20 @@ class Solution:
         return self.slidingWindowAtMost(nums, k) - self.slidingWindowAtMost(nums, k - 1)
 
     def slidingWindowAtMost(self, nums: List[int], distinctK: int) -> int:
-        freq_map = {}
+        freq = {}
         left = 0
         total_count = 0
 
         for right in range(len(nums)):
-            freq_map[nums[right]] += 1
+            if nums[right] in freq:
+                freq[nums[right]]+=1
+            else:
+                freq[nums[right]]=1
 
-            while len(freq_map) > distinctK:
-                freq_map[nums[left]] -= 1
-                if freq_map[nums[left]] == 0:
-                    del freq_map[nums[left]]
+            while len(freq) > distinctK:
+                freq[nums[left]] -= 1
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
                 left += 1
 
             total_count += right - left + 1
