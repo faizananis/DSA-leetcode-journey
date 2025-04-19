799bfc9e0108411e7d48778ce86e6805d4c18c79
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-04-20 04:56:56 +0500

    Time: 51 ms (72.05%), Space: 60.6 MB (7.3%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,16 +1,25 @@
 class Solution {
-    #define ll long long
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
+        int n = nums.size();
 
-        ll ans=0;
-        for(int i=0;i<n-1;++i){
-            int lb = lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i])-nums.begin();
-            int ub = upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i])-nums.begin();
-            ans += (ub - lb);
+        sort(begin(nums), end(nums)); //nlogn
+
+        long long result = 0;
+
+        for(int i = 0; i < n; i++) { //O(n * logn)
+             //o(n)
+            int idx = lower_bound(begin(nums) + i + 1, end(nums), lower-nums[i]) - begin(nums); //Gives the firstc element NOT LESS than (lower - nums[i]) //log(n)
+            int x   = idx - 1 - i;
+
+            idx = upper_bound(begin(nums) + i + 1, end(nums), upper-nums[i]) - begin(nums); //Gives the firstc element GREATER than (upper - nums[i]) //log(n)
+
+            int y = idx - 1 - i;
+
+            result += (y-x);
         }
-        return ans;
+
+        return result;
     }
 };
+
