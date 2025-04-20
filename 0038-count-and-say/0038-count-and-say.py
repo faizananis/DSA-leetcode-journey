ca506b4d3d9419cd98e54fec071b34524675babb
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-04-21 01:02:15 +0500

    Time: 11 ms (21.64%), Space: 17.9 MB (60.93%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,16 +2,22 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
-        return self.converter(self.countAndSay(n - 1))
+        
+        say = self.countAndSay(n-1)
+        
+        result = ""
 
-    def converter(self, num: str) -> str:
-        i = 0
-        res = ""
-        while i < len(num):
-            cnt = 1
-            while i + 1 < len(num) and num[i] == num[i + 1]:
-                cnt += 1
-                i += 1
-            res += str(cnt) + num[i]
-            i += 1
-        return res
+        i=0
+        while i<len(say):
+            ch = say[i]
+
+            count = 1
+
+            while i<len(say)-1 and say[i]==say[i+1]:
+                count+=1
+                i+=1
+            result+=str(count)+say[i]
+            i+=1
+            
+        return result
+
