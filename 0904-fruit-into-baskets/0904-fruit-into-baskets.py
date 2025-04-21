370a97be8f7554383bbfd855e0bfdf7e9ff945be
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-04-21 10:24:09 +0500

    Time: 143 ms (86.28%), Space: 23.3 MB (97.82%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,43 +1,43 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        i, j = 0, 0
-        n = len(fruits)
-        while j < n and fruits[i] == fruits[j]:
-            j += 1
-        if j == n:
-            return n
+        # i, j = 0, 0
+        # n = len(fruits)
+        # while j < n and fruits[i] == fruits[j]:
+        #     j += 1
+        # if j == n:
+        #     return n
 
-        l, r = i, j + 1
-        ans = 0
-        while r < n:
-            if fruits[r] != fruits[j]:
-                if fruits[r] == fruits[i]:
-                    i = j
-                    j = r
-                else:
-                    ans = max(ans, r - l)
-                    i = j
-                    l = j
-                    j = r
-            r += 1
-        ans = max(ans, r - l)
-        return ans
-        # j=0
-        # maxlength=0
-        # table={}
-        # for i in range(len(fruits)):
-        #     if fruits[i] not in table:
-        #         table[fruits[i]]=1
-        #     else:
-        #         table[fruits[i]]+=1
-        #     if len(table)<=2:
-        #         maxlength=max(maxlength,i-j+1)
-        #     else:
-        #         table[fruits[j]]-=1
-        #         if table[fruits[j]]==0:
-        #             table.pop(fruits[j])
-        #         j+=1
-        # return maxlength
+        # l, r = i, j + 1
+        # ans = 0
+        # while r < n:
+        #     if fruits[r] != fruits[j]:
+        #         if fruits[r] == fruits[i]:
+        #             i = j
+        #             j = r
+        #         else:
+        #             ans = max(ans, r - l)
+        #             i = j
+        #             l = j
+        #             j = r
+        #     r += 1
+        # ans = max(ans, r - l)
+        # return ans
+        left=0
+        maxlength=0
+        table={}
+        for right in range(len(fruits)):
+            if fruits[right] not in table:
+                table[fruits[right]]=1
+            else:
+                table[fruits[right]]+=1
+            if len(table)<=2:
+                maxlength=max(maxlength,right-left+1)
+            else:
+                table[fruits[left]]-=1
+                if table[fruits[left]]==0:
+                    table.pop(fruits[left])
+                left+=1
+        return maxlength
             
             
                 
