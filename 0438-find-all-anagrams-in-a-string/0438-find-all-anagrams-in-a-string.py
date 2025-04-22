574c8ef18032d68e89b422c304cb42da4bab5dd1
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-04-23 01:11:03 +0500

    Time: 59 ms (52.69%), Space: 18.6 MB (16.4%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,14 +1,6 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s):
-            return []
-        p_freq=Counter(p)
-        # for c in p:
-        #     if c not in p_freq:
-        #         p_freq[c]=1
-        #     else:
-        #         p_freq[c]+=1
-        
+        p_freq=Counter(p)    
         left=0
         right=0
         window={}
@@ -22,11 +14,13 @@ class Solution:
 
             if (right-left+1)<len(p):
                 continue
+
             elif right-left+1==len(p):
                 if window==p_freq:
                     ans.append(left)
+                    
             else:
-                window[s[left]]-=1
+                window[s[left]]=window[s[left]]-1
                 if window[s[left]]==0:
                     del window[s[left]]
                 left+=1
