ea6245b4fa377279799c1ac2d5497b4f39870bb0
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-04-24 04:37:05 +0500

    Time: 34 ms (58.64%), Space: 17.9 MB (41.12%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.py
+++ 1399-count-largest-group/1399-count-largest-group.py
@@ -1,3 +1,21 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        return len(multimode(sum(map(int,str(v))) for v in range(1,n+1)))
+        my_dict={}
+        max_size=0
+        for i in range(1,n+1):
+            num=i
+            sums=0
+            while num:
+                sums+=(num%10)
+                num//=10
+            if sums not in my_dict:
+                my_dict[sums]=1
+            else:
+                my_dict[sums]+=1
+            max_size=max(max_size,my_dict[sums])
+        
+        count=0
+        for n in my_dict:
+            if my_dict[n]==max_size:
+                count+=1
+        return count
