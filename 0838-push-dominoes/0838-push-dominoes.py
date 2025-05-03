e5d95603d88e77d8f45416a6d8032730d8a45975
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-05-03 06:27:23 +0500

    Time: 289 ms (17.49%), Space: 23.4 MB (36.63%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.py
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -1,24 +1,41 @@
-class Solution(object):
-    def pushDominoes(self, dominoes):
-        N = len(dominoes)
-        force = [0] * N
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        n=len(dominoes)
+        leftClosest=[-1]*n
+        rightClosest=[-1]*n
+        result=""
 
-        # Populate forces going from left to right
-        f = 0
-        for i in range(N):
-            if dominoes[i] == 'R': f = N
-            elif dominoes[i] == 'L': f = 0
-            else: f = max(f-1, 0)
-            force[i] += f
-
-        # Populate forces going from right to left
-        f = 0
-        for i in range(N-1, -1, -1):
-            if dominoes[i] == 'L': f = N
-            elif dominoes[i] == 'R': f = 0
-            else: f = max(f-1, 0)
-            force[i] -= f
-
-        return "".join('.' if f==0 else 'R' if f > 0 else 'L'
-                       for f in force)
+        for i in range(n):
+            if dominoes[i]=='R':
+                leftClosest[i]=i
+            elif dominoes[i]=='L':
+                leftClosest[i]=-1
+            elif i>0:
+                leftClosest[i]=leftClosest[i-1]
         
+        for i in range(n-1,-1,-1):
+            if dominoes[i]=='L':
+                rightClosest[i]=i
+            elif dominoes[i]=='R':
+                rightClosest[i]=-1
+            elif i<n-1:
+                rightClosest[i]=rightClosest[i+1]
+        
+        print(leftClosest)
+        print(rightClosest)
+        for i in range(n):
+            if leftClosest[i]==rightClosest[i]==-1:
+                result+='.'
+            elif leftClosest[i]==-1:
+                result+='L'
+            elif rightClosest[i]==-1:
+                result+='R'
+            else:
+                distLeft=abs(i-leftClosest[i])
+                distRight=abs(i-rightClosest[i])
+                if distLeft==distRight:
+                    result+='.'
+                else:
+                    result+='R' if distLeft<distRight else 'L'
+
+        return result
