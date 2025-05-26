1f15b9fc5adf74e8c653420d4106735023787d18
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-05-26 15:56:24 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self,text1: str, text2: str) -> int:
-        @lru_cache(maxsize=None)
+        
+
         def dp(i,j):
             # Base Case: reached the end of either string
             if i == len(text1) or j == len(text2):
                 return 0
             # Characters match
             if text1[i] == text2[j]:
-                print(text1[i],text2[j])
                 return 1 + dp(i + 1, j + 1)
             # Try skipping either character
             else:
