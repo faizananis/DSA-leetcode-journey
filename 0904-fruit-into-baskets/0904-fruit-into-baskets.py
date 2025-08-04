99e404cabc33921e0b455c74bda655a3c169b5ec
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-08-04 08:37:27 +0500

    Time: 195 ms (31.93%), Space: 23.6 MB (42.94%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,44 +1,17 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        # i, j = 0, 0
-        # n = len(fruits)
-        # while j < n and fruits[i] == fruits[j]:
-        #     j += 1
-        # if j == n:
-        #     return n
-
-        # l, r = i, j + 1
-        # ans = 0
-        # while r < n:
-        #     if fruits[r] != fruits[j]:
-        #         if fruits[r] == fruits[i]:
-        #             i = j
-        #             j = r
-        #         else:
-        #             ans = max(ans, r - l)
-        #             i = j
-        #             l = j
-        #             j = r
-        #     r += 1
-        # ans = max(ans, r - l)
-        # return ans
         left=0
-        maxlength=0
-        table={}
+        my_dict={}
+        max_size=0
         for right in range(len(fruits)):
-            if fruits[right] not in table:
-                table[fruits[right]]=1
+            if fruits[right] not in my_dict:
+                my_dict[fruits[right]]=1
             else:
-                table[fruits[right]]+=1
-            if len(table)<=2:
-                maxlength=max(maxlength,right-left+1)
-            else:
-                table[fruits[left]]-=1
-                if table[fruits[left]]==0:
-                    table.pop(fruits[left])
+                my_dict[fruits[right]]+=1
+            while len(my_dict)>2 and left<right:
+                my_dict[fruits[left]]-=1
+                if my_dict[fruits[left]]==0:
+                    del my_dict[fruits[left]]
                 left+=1
-        return maxlength
-            
-            
-                
-
+            max_size=max(max_size,right-left+1)
+        return max_size
