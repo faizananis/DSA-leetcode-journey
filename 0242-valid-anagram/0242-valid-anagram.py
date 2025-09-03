99f7b9e173e84cd9c53bb94840d17b5f9dd41268
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-09-03 10:53:13 +0500

    Time: 40 ms (6.33%), Space: 16.8 MB (100%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        arr=[0]*26
+        for char in s:
+            arr[ord(char)-97]+=1
+        for char in t:
+            arr[ord(char)-97]-=1
+        for i in range(26):
+            if arr[i]!=0:
+                return False
+        return True
