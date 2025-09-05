626bedda2bec75120f197dfc2650e2d71d13c68d
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-09-05 11:35:14 +0500

    Time: 7 ms (89.16%), Space: 17.8 MB (64.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        arr=[0]*26
-        for char in s:
-            arr[ord(char)-97]+=1
-        for char in t:
-            arr[ord(char)-97]-=1
-        for i in range(26):
-            if arr[i]!=0:
-                return False
-        return True
+        dict1=Counter(s)
+        dict2=Counter(t)
+        return dict1==dict2
