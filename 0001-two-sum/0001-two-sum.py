357ac4012c8de1a7571d7a8fcffc1946e73bbc29
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-11-09 12:30:57 +0500

    Time: 0 ms (100%), Space: 18.8 MB (62.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,4 +4,25 @@ class Solution:
         for i in range(len(nums)):
             if target-nums[i] in my_dict:
                 return [my_dict[target-nums[i]],i]
-            my_dict[nums[i]]=i
+            else:
+                my_dict[nums[i]]=i
+        
+            
+
+
+
+
+
+
+
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j]==target:
+        #             return [i,j]
+        
+        # my_dict={}
+        # for i in range(len(nums)):
+        #     if target-nums[i] in my_dict:
+        #         return [my_dict[target-nums[i]],i]
+        #     my_dict[nums[i]]=i
