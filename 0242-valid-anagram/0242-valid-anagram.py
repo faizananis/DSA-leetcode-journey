ecbffdc7bef544a73272130f2fc0fec692290625
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2025-11-24 07:51:21 +0500

    Time: 17 ms (19.85%), Space: 17.9 MB (48.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,22 @@
 class Solution:
+    def hashFunction(self,n):
+        return n-97
     def isAnagram(self, s: str, t: str) -> bool:
-        dict1=Counter(s)
-        dict2=Counter(t)
-        return dict1==dict2
+        arr1=[0]*26
+        arr2=[0]*26
+        if len(s)!=len(t):
+            return False
+        for i in range(len(s)):
+
+            index1=self.hashFunction(ord(s[i]))
+            arr1[index1]+=1
+
+            index2=self.hashFunction(ord(t[i]))
+            arr2[index2]+=1
+
+        for i in range(26):
+            if arr1[i]!=arr2[i]:
+                return False
+        return True
+
+
