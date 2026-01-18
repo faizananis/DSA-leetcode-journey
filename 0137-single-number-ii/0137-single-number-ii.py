f38c8b1f21d20c320d2aa73f581c2c60f68fe9ad
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2026-01-19 04:57:10 +0500

    Time: 3 ms (72.67%), Space: 20.8 MB (11.3%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,12 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result=0
-        for i in range(32):
-            bitsum=0
-            for n in nums:
-                bitsum+=(n>>i)&1
-            if bitsum%3:
-                result|=(1<<i)
-        if result>2**31-1:
-            result-=2*2**31
-        return result
+        ones=0
+        twos=0
+        for n in nums:
+            ones^=n&(~twos)
+            twos^=n&(~ones)
+        
+        return ones
