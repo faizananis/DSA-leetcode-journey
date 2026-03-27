f1448092ce893e490934b1708d3f36795e00bc78
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2026-03-27 08:49:59 +0500

    Time: 574 ms (15.12%), Space: 34.9 MB (97.67%) - LeetHub

---
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countExcellentPairs(self, nums: List[int], k: int) -> int:
+        hash_map={}
+        my_set=set(nums)
+        for i in my_set:
+            n=i
+            count=0
+            while n:
+               count+=n&1
+               n=n>>1
+            if count not in hash_map:
+                hash_map[count]=1
+            else:
+                hash_map[count]+=1
+
+        result=0
+
+        for i in hash_map:
+            for j in hash_map:
+                if i+j>=k:
+                    result+=hash_map[i]*hash_map[j]
+        
+        return result
