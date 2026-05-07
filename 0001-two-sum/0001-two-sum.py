d69205cf423218651dc117635760a87be7a44c5c
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2026-05-07 18:54:54 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        my_dict={}
+        count={}
         for i in range(len(nums)):
-            if target-nums[i] in my_dict:
-                return [my_dict[target-nums[i]],i]
+            if target-nums[i] in count:
+                return [count[target-nums[i]],i]
             else:
-                my_dict[nums[i]]=i
+                count[nums[i]]=i
         
             
 
