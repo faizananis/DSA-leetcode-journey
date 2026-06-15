66a0f50f09a0a72b614558044f5f6588b7207d90
Muhammad Faizan Anis <muhammadfaizananis@gmail.com>
2026-06-15 18:43:17 +0500

    Time: 85 ms (67.04%), Space: 63 MB (11.9%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -5,45 +5,14 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = head
-        slow = head
-        fast = head
-        if head.next == None:
+        slow=head
+        fast=head
+        prev=None
+        if head.next==None:
             return None
         while fast and fast.next:
-            prev = slow
-            slow  = slow.next
-            fast = fast.next.next
-
-        temp=slow
-        prev.next = slow.next
-        temp.next=None
+            prev=slow
+            slow=slow.next
+            fast=fast.next.next
+        prev.next=prev.next.next
         return head
-        # if head.next==None:
-        #     return None
-        
-        # slow = head
-        # fast = head
-        
-        # while fast and fast.next:
-        #     fast = fast.next.next
-        #     if fast and fast.next:
-        #         slow = slow.next
-        
-        # slow.next = slow.next.next
-        
-        # return head
-    
-    # def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    #     turtle, hare = head, head
-    #     while hare.next and hare.next.next and hare.next.next.next != None:
-    #         turtle = turtle.next
-    #         hare = hare.next.next
-        
-    #     if head.next:
-    #         temp = turtle.next.next
-    #         turtle.next.next = None
-    #         turtle.next = temp
-    #         return head
-
-        
